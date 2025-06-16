5a6bd29fe1380b161bdffe37ad1bae9113a3830b
volosova <poluslera@yandex.ru>
2025-06-16 17:27:43 +0300

    Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright,Playwright, Page
 
 @pytest.fixture
-def chromium_page():
+def chromium_page(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
