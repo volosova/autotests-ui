29e6654321ee8493131255e5db6188b0504f90df
volosova <poluslera@yandex.ru>
2025-07-31 18:57:06 +0300

    Параметризация Pytest.Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,7 +3,8 @@ from playwright.sync_api import sync_playwright, expect
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authotization():
+@pytest.mark.parametrize("email, password", [("user.name@gmail.com", "password"),("user.name@gmail.com", "  "),("  ", "password")], ids=["Invalid email and password", "Invalid email and empty password", "Empty email and invalid password"])
+def test_wrong_email_or_password_authorization(email: str, password: str):
 
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
@@ -12,10 +13,10 @@ def test_wrong_email_or_password_authotization():
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
         email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        email_input.fill("user.name@gmail.com")
+        email_input.fill(email)
 
         password_input = page.get_by_test_id('login-form-password-input').locator('input')
-        password_input.fill("password")
+        password_input.fill(password)
 
         login_button = page.get_by_test_id('login-page-login-button')
         login_button.click()
