8c14d6de4632000d39dee6752cb9bdb51c1f07bc
volosova <poluslera@yandex.ru>
2025-08-15 16:19:00 +0300

    Знакомство с PageObject.Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,12 +5,10 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-@pytest.mark.parametrize("email, username, password", [("user.name@gmail.com", "username", "password")])
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str, username: str, password: str):
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
 
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.fill_registration_form(email=email, username=username, password=password)
+        registration_page.fill_registration_form(email="user.name@gmail.com", username="username", password="password")
         registration_page.click_registration_button()
 
-        dashboard_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
         dashboard_page.check_dashboard_title()
