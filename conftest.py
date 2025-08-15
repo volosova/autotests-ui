ae8b2ca59d4ea24fe918d0a31a82d0d1ab4c8553
volosova <poluslera@yandex.ru>
2025-08-15 12:11:55 +0300

    Знакомство с PageObject.Практика работы с Page Object

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
 pytest_plugins = (
-    "fixtures.pages"
+    "fixtures.pages",
     "fixtures.browsers"
 )
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,36 +1,16 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():
+@pytest.mark.parametrize("email, username, password", [("user.name@gmail.com", "username", "password")])
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str, username: str, password: str):
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
- 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.fill_registration_form(email=email, username=username, password=password)
+        registration_page.click_registration_button()
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
- 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path='browser-state.json')
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-
-        page.wait_for_timeout(5000)
+        dashboard_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        dashboard_page.check_dashboard_title()
