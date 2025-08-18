fc61e42ed11825d9b82a98ea29477975ae95e1a3
volosova <poluslera@yandex.ru>
2025-08-18 14:43:19 +0300

    Создание страниц Dashboard и CoursesList

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -5,6 +5,7 @@ from playwright.sync_api import Page
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
+from pages.courses_list_page import CoursesListPage
 
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
@@ -17,3 +18,7 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 @pytest.fixture
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
